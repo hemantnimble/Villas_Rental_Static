c94f5e7830bd5b748cb49ceb45cab55c87df621f
Hemant Nimble <nimblehemant@gmail.com>
2024-01-19 18:37:55 +0530

    Google Sheets Api

--- Js/index.js
+++ Js/index.js
@@ -43,7 +43,7 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.about-main', { delay: 200, origin: 'bottom' });
-ScrollReveal().reveal('.about-discription', { delay: 200,  distance: '20px', origin: 'left' });
+ScrollReveal().reveal('.about-discription', { delay: 200, distance: '20px', origin: 'left' });
 ScrollReveal().reveal('.about-dis img', { delay: 200, origin: 'bottom' });
 ScrollReveal().reveal('.about-dis p', { delay: 200, origin: 'bottom' });
 ScrollReveal().reveal('.lonavala-right-side', { delay: 200, origin: 'right' });
@@ -58,3 +58,27 @@ var loader = document.getElementById("loader-main");
 window.addEventListener("load", function () {
     loader.style.display = "none";
 })
+
+//GOOGLE SHEET API
+
+const spreadsheetId = '1AZ5YSK29pQa5pNNXtm2TgRsod0pcS6st06_iOVUNz98';
+const range = 'Sheet1!A:Z';
+const apiKey = 'AIzaSyCMRdod9AS4x-g4LJu5VN9VAhP8QVVNQd8';
+
+const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+        const heramb = document.getElementById('heramb');
+        heramb.textContent = data.values[1][1];
+
+        const anjum = document.getElementById('anjum');
+        anjum.textContent = data.values[2][1];
+
+        const jannat = document.getElementById('jannat');
+        jannat.textContent = data.values[3][1];
+    })
+    .catch(error => console.error('Error fetching data:', error));
+
